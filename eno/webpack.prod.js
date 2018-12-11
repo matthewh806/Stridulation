76ede1519d044143c81cc947fbb37f1f73c1b342
matthewh806 <matthewh806@gmail.com>
2018-12-11 23:54:02 +0100

    automate step of uploading to s3

--- eno/webpack.prod.js
+++ eno/webpack.prod.js
@@ -1,7 +1,24 @@
+const path = require('path');
+const AWS = require("aws-sdk");
 const merge = require('webpack-merge');
+const s3Plugin = require('webpack-s3-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
 	mode: 'production',
-	devtool: 'source-map'
+	devtool: 'source-map',
+
+	plugins: [
+		new s3Plugin({
+			s3Options: {
+				credentials: new AWS.SharedIniFileCredentials(),
+				// directory: path.resolve(__dirname, 'build'),
+				basePath: '',
+				region: 'us-west-2'
+			},
+			s3UploadOptions: {
+				Bucket: 'matthewdharris.net'
+			}
+		})
+	]
 });
